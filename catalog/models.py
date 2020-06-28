865546ba7058b0cead14383881911c43cc1b94d6
buzzon <buzzondev@gmail.com>
2020-06-28 21:00:46 +0500

    admin panel

--- catalog/models.py
+++ catalog/models.py
@@ -16,7 +16,7 @@ class Language(models.Model):
 
 class Book(models.Model):
 	title = models.CharField(max_length=200)
-	autor = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+	author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
 	summary = models.TextField(max_length=1000, help_text="nter a brief description of the book")
 	isbn = models.CharField('ISBN',max_length=13, help_text="13 Character <a href=\"https://www.isbn-international.org/content/what-isbn\">ISBN number</a>")
 	genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
@@ -27,6 +27,10 @@ class Book(models.Model):
 
 	def get_absolute_url(self):
 		return reverse('book-detail', args=[str(self.id)])
+
+	def display_genre(self):
+		return ', '.join([ genre.name for genre in self.genre.all()[:3] ])
+	display_genre.short_description = 'Genre'
 
 class BookInstance(models.Model):
 	id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for this particular book across whole library")
